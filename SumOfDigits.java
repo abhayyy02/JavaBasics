7132fb87b1e5d96c79e4806aa2b0c310b1402d48
Abhay Jadhav <abhayjadhav1221@gmail.com>
2024-03-13 15:29:12 +0530

    Add files via upload

---
+++ SumOfDigits.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class SumOfDigit
+{
+	public static void main (String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int num = sc.nextInt();
+		int dup = num;
+		int sum = 0;
+		sum = sum + num%10;
+		num = num/10;
+		sum = sum + num%10;
+		num = num/10;
+		sum = sum + num%10;
+		System.out.println("Sum of digit is : " +sum+ " And the numbber is : " +dup);
+
+	}
+
+
+
+
+
+}
