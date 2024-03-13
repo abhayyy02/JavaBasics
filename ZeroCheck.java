56e1a2e62c88bdd0b7171a7983bcb2a886abdf53
Abhay Jadhav <abhayjadhav1221@gmail.com>
2024-03-13 15:29:46 +0530

    Add files via upload

---
+++ ZeroCheck.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class ZeroCheck
+{
+	public static void main (String[]args)
+	{
+		
+		int num1 = 0;
+		
+		int num2 = (num1==0)?0:num1;
+
+		System.out.println (num2);
+		
+
+
+	}
+
+
+}
