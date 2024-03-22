f5cfc0df28ff4bc97f41794acec92551c9241750
Abhay Jadhav <abhayjadhav1221@gmail.com>
2024-03-23 02:25:33 +0530

    Length_of_Number

--- While_PowerandLength.java
+++ While_PowerandLength.java
@@ -1,28 +1,20 @@
 import java.util.Scanner;
-class While_PowerandLength
+class While_NumofDigits
 {
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a Number :");
-
 		int num = sc.nextInt();
-		int dup = num;
+		
 		int length = 0;
-		int power = 1;
-
 		while(num != 0)
 		{
 			length++;
 			num/=10;
 		}
 
-		for(int i=0;i<=length;i++)
-		{
-			power *= dup;
-		}
-
-		System.out.println("The power is " +power);
+		
 		System.out.println("The length of num is " +length);
 	}
 }
